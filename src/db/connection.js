5beb0145365825ec4ad778c3fdd6eb04fb61fd24
DzidediSenaya <dzidedisenaya@gmail.com>
2024-05-03 16:57:53 +0000

    cx

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async () => {
   try {
-    await mongoose.connect(process.env.ATLAS_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully!");
   } catch (error) {
     console.log(error);
--- src/server/app.js
+++ src/server/app.js
@@ -3,28 +3,32 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import Connection from "../db/connection.js";
 import authRoutes from "../routes/authroutes.js";
-import userRoutes from "../routes/userRoutes.js";
+import userRoutes from "../routes/userroutes.js";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-//add middlewares
+// add middlewares
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "http://localhost:3000",
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//add routes
+// add routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-//set up mongo db connection
+// set up MongoDB connection
 Connection();
 
-//listening to server connection
+// listening to server connection
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
